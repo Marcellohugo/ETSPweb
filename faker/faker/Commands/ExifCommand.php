9928afc90f2c96e1eb9ca451232ba90f755ae439
joyazovancha <145383781+joyazovancha@users.noreply.github.com>
2023-10-27 02:56:37 +0700

    Add files via upload

--- faker/faker/Commands/ExifCommand.php
+++ faker/faker/Commands/ExifCommand.php
@@ -1,61 +1,63 @@
 <?php
 
-namespace Intervention\Image\Commands;
+namespace Intervention\Image\Imagick\Commands;
 
-use Intervention\Image\Exception\NotReadableException;
+use Intervention\Image\Commands\ExifCommand as BaseCommand;
 use Intervention\Image\Exception\NotSupportedException;
 
-class ExifCommand extends AbstractCommand
+class ExifCommand extends BaseCommand
 {
     /**
-     * Read Exif data from the given image
+     * Prefer extension or not
      *
-     * Note: Windows PHP Users - in order to use this method you will need to
-     * enable the mbstring and exif extensions within the php.ini file.
+     * @var bool
+     */
+    private $preferExtension = true;
+
+    /**
+     *
+     */
+    public function dontPreferExtension() {
+        $this->preferExtension = false;
+    }
+
+    /**
+     * Read Exif data from the given image
      *
      * @param  \Intervention\Image\Image $image
      * @return boolean
      */
     public function execute($image)
     {
-        if (!function_exists('exif_read_data')) {
+        if ($this->preferExtension && function_exists('exif_read_data')) {
+            return parent::execute($image);
+        }
+
+        $core = $image->getCore();
+
+        if ( ! method_exists($core, 'getImageProperties')) {
             throw new NotSupportedException(
                 "Reading Exif data is not supported by this PHP installation."
             );
         }
 
-        $key = $this->argument(0)->value();
-
-        // try to read exif data from image file
-        try {
-            if ($image->dirname && $image->basename) {
-                $stream = $image->dirname . '/' . $image->basename;
-            } elseif (version_compare(PHP_VERSION, '7.2.0', '>=')) {
-                // https://www.php.net/manual/en/function.exif-read-data.php#refsect1-function.exif-read-data-changelog
-                $stream = $image->stream()->detach();
-            } else {
-                // https://bugs.php.net/bug.php?id=65187
-                $stream = $image->encode('data-url')->encoded;
-            }
-
-            $data = @exif_read_data($stream);
-
-            if (!is_null($key) && is_array($data)) {
-                $data = array_key_exists($key, $data) ? $data[$key] : false;
-            }
-
-        } catch (\Exception $e) {
-            throw new NotReadableException(
-                sprintf(
-                    "Cannot read the Exif data from the filename (%s) provided ",
-                    $image->dirname . '/' . $image->basename
-                ),
-                $e->getCode(),
-                $e
-            );
+        $requestedKey = $this->argument(0)->value();
+        if ($requestedKey !== null) {
+            $this->setOutput($core->getImageProperty('exif:' . $requestedKey));
+            return true;
         }
 
-        $this->setOutput($data);
+        $exif = [];
+        $properties = $core->getImageProperties();
+        foreach ($properties as $key => $value) {
+            if (substr($key, 0, 5) !== 'exif:') {
+                continue;
+            }
+
+            $exif[substr($key, 5)] = $value;
+        }
+
+        $this->setOutput($exif);
         return true;
     }
 }
